352b5e314298ae68659e4a8be021ee841a2b53c1
Michał Boruch <michal.boruch@skyverse.co>
2022-03-07 17:14:20 +0100

    feat: routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import './App.css';
+import { PageRoute } from 'components';
+import { routes } from 'constants/routes.constants';
+
+import './assets/styles/app.scss';
 
 export const App: React.FC = () => {
   return (
-    <div className="App">
-      <p>App</p>
-    </div>
+    <Routes>
+      {routes.map((route) => (
+        <Route key={route.name} path={route.path} element={<PageRoute {...route} />} />
+      ))}
+    </Routes>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import { App } from 'app';
 import { reportWebVitals } from 'reportWebVitals';
 
-import './index.css';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
