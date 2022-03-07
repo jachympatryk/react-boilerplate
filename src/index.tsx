bf50fa7fbaa17e9ee0c548d54259ecb1d893f4fe
Michał Boruch <michal.boruch@skyverse.co>
2022-03-07 16:44:54 +0100

    config: add lint run scripts

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from 'app';
-import reportWebVitals from './reportWebVitals';
+import { reportWebVitals } from 'reportWebVitals';
 
 import './index.css';
 
