b51b900a9cb94f5cdbd5c2cee88295f057b69192
Michał Boruch <miboruch@gmail.com>
2022-09-29 09:28:04 +0200

    feat: scss variables

--- src/server/builder.ts
+++ src/server/builder.ts
@@ -1,35 +1,34 @@
-import { FetchBuilder } from '@better-typed/hyper-fetch';
+import { Builder } from "@better-typed/hyper-fetch";
 
-import { ServerErrorType } from './server.types';
-import { environment } from 'config/environment.config';
-import { REFRESH_TOKEN_STORAGE_FIELD } from 'constants/auth.constants';
+import { ServerErrorType } from "./server.types";
+import { environment } from "config/environment.config";
+import { REFRESH_TOKEN_STORAGE_FIELD } from "constants/auth.constants";
 
-export const builder = new FetchBuilder<ServerErrorType>({ baseUrl: environment.serverUrl })
+export const builder = new Builder<ServerErrorType>({ baseUrl: environment.serverUrl })
   .setDebug(true)
   .onError(async (res, command) => {
     const [, , status] = res;
     const refreshToken = localStorage.getItem(REFRESH_TOKEN_STORAGE_FIELD);
-    const isLoginEndpoint = command.endpoint.includes('login');
+    const isLoginEndpoint = command.endpoint.includes("login");
 
     if (status === 401 && !isLoginEndpoint && !command.used && refreshToken) {
       /* handle refresh token request */
     }
     return res;
   })
-  .setQueryParamsOptions({
-    arrayFormat: 'comma',
+  .setQueryParamsConfig({
+    arrayFormat: "comma",
     skipEmptyString: true,
   })
   .onAuth((command) => {
     /*
-      const state = store.getState();
-      const authToken = state.auth.token;
-    */
-    const authToken = '';
+          const state = store.getState();
+          const authToken = state.auth.token;
+        */
+    const authToken = "";
 
     return command.setHeaders({
       ...command.headers,
       Authorization: `Bearer ${authToken}`,
     });
-  })
-  .build();
+  });
